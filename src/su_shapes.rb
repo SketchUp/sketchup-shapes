630547b35a43acd0932982f48321cafcb7f823ec
John McClenahan <john.mcclenahan@gmail.com>
2014-01-31 15:51:59 +0000

    Second attempt to commit my updated su_shapes.rb and su_shapes/shapes.rb to my remote GitHub repository, in the root of the folder

--- src/su_shapes.rb
+++ src/su_shapes.rb
@@ -1,12 +1,19 @@
-# Copyright 2014 Trimble Navigation Ltd.
-#
-# License: The MIT License (MIT)
-#
-# A SketchUp Ruby Extension that creates simple shape objects.  More info at  
-# https://github.com/SketchUp/shapes
+# Original v1.0 Copyright 2013, Trimble Navigation Limited
+# Subsequent versions 1.1 and later Copyright 2013 John W McClenahan
+
+# The original version 1.0 software was provided as an example of using the Ruby interface
+# to SketchUp.
+
+# Permission to use, copy, modify, and distribute this software for 
+# any purpose and without fee is hereby granted, provided that the above
+# copyright notice appear in all copies.
+
+# THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #-----------------------------------------------------------------------------
-# Name        :   Shapes 1.0
-# Description :   Classes for creating and editing parametric shapes
+# Name        :   Shapes 1.4.1
+# Description :   Classes for creating and editing parametric shapes from the Sketchup Draw/Shapes menu
 # Menu Item   :   Draw->Shapes->Box
 #             :   Draw->Shapes->Cylinder
 #             :   Draw->Shapes->Cone
@@ -15,27 +22,66 @@
 #             :   Draw->Shapes->Prism
 #             :   Draw->Shapes->Pyramid
 #             :   Draw->Shapes->Dome
-# Context Menu:   Edit Box|Cylinder|Cone|Torus|Tube|Prism|Pyramid|Dome
-# Usage       :   Select desired shape and fill in the dialog box that opens.
-# Date        :   9/14/2004
-# Type        :   Dialog Box
+#             :   Draw->Shapes->Sphere
+# Context Menu:   Edit Box|Cylinder|Cone|Torus|Tube|Prism|Pyramid|Dome|Sphere
+# Usage       :   Select desired shape and fill in the dialogue box that opens.
+# Date        :   Original 2004-09-14. Latest revision 2014-01-29
+# Type        :   Dialogue Box
+#-----------------------------------------------------------------------------
+# Modified to v1.1 J W McClenahan 2013-09-14
+# Added parameter for number of segments to draw for Cylinder, Cone, Torus and  Tube, replacing fixed n=24.
+# Changed Box default height 
+#-----------------------------------------------------------------------------
+# Modified to v1.2 J W McClenahan 2013-09-15
+# Added code to detect model length units, and set different default sizes for imperial and metric units 
+#   in round numbers in each unit system - e.g, substituting 300mm for 1 foot
+# Tricky bit was finding where to set module-wide values accessible within each class - copied example 
+# of points_in_a_circle function to define a function which returns length_unit as a value (now unit_length)
+#-----------------------------------------------------------------------------
+#  Modified to v1.3 J W McClenahan 2013-09-20
+#   Make initial default sizes one unit in each dimension in model units and format, 
+#     or simple x10 multiple of one unit (for mm and cm units)
+#   Now remembers last size chosen for shape and presents it as default next time
+#		Extended Pyramid to n-gon base, not just square base (requires 3 sides or more in base polygon)
+#		Added Class to construct Sphere as well as Dome.
+#-----------------------------------------------------------------------------
+#		Modified to v1.4 J W McClenahan 2013-09-22
+#			Redefined torus big radius so it becomes the overall outer radius defaulting to one unit, 
+#			not as at present where it is the radius to the centre of the smaller radius. 
+#			Amended torus validation check so that small radius must be no more than half outside radius.
+#			Changed small radius default to half outer radius, giving a torus with no central hole
+#-----------------------------------------------------------------------------
+#		Modified to v1.4.1 J W McClenahan 2013-12-31
+#			Adapting to SU2014 Beta - Ruby changed to v2.0 and shapes.rb crashes - won't load
+#			All lines like: 
+#				defaults = {"r", 2.feet, "h", 4.feet, "n", 6} ## Original values,
+#			or
+#			 defaults = {"w", @@dim1, "d", @@dim2, "h", @@dim3} ## My amended versions
+#			failed with syntax error 
+#			Changed all such lines to the now necessary syntax form for defining a hash, 
+#				with remembered default values:
+#				defaults = {"w" => @@dim1, "d" => @@dim2, "h" => @@dim3} 
+#			Following open sourcing of the program by Sketchup, renamed module from JWM back to original
+#				and renamed files and subfolder from jwm_shapes back to su_shapes
 #-----------------------------------------------------------------------------
 
 require 'sketchup.rb'
 require 'extensions.rb'
 
 module Sketchup::Samples
+# module JWM
 module Shapes
 
 # Create the extension.
 shapes_ext = SketchupExtension.new 'Shapes Tool', 'su_shapes/shapes.rb'
-shapes_ext.description = 'Shapes sample script from SketchUp.com'
-shapes_ext.version =  '1.1.2'
-shapes_ext.creator = "SketchUp"
-shapes_ext.copyright = "2014, Trimble Navigation Limited"
+shapes_ext.description = 'Enhanced Shapes based on original script from SketchUp.com'
+shapes_ext.version =  '1.4.1'
+shapes_ext.creator = "Sketchup and John McClenahan"
+shapes_ext.copyright = "2013, John McClenahan and Trimble Navigation Limited Sketchup Team "
 
 # Register the extension with Sketchup so it show up in the Preference panel.
 Sketchup.register_extension shapes_ext, true
 
 end # module Shapes
+# end module JWM
 end # module Sketchup::Samples
--- src/su_shapes/mesh_additions.rb
+++ src/su_shapes/mesh_additions.rb
@@ -1,24 +1,15 @@
-# The MIT License (MIT)
+# Copyright 2013, Trimble Navigation Limited
 
-# Copyright (c) 2014 Trimble Navigation Ltd.
+# This software is provided as an example of using the Ruby interface
+# to SketchUp.
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Permission to use, copy, modify, and distribute this software for 
+# any purpose and without fee is hereby granted, provided that the above
+# copyright notice appear in all copies.
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 #-----------------------------------------------------------------------------
 # Name        :   Mesh Additions 1.0
 # Description :   This file defines some additional methods for Geom::PoygonMesh
@@ -45,12 +36,12 @@ def add_revolved_points(pts, axis, numsegments)
 
     #TODO: Determine if the points are all in the same plane as the axis
     planar = true
-
+    
     # Create a transformation that will revolve the points
     angle = Math::PI * 2
     da = angle / numsegments
     t = Geom::Transformation.rotation(axis[0], axis[1], da)
-
+    
     # Add the points to the mesh
     index_array = []
     for pt in pts do
@@ -66,7 +57,7 @@ def add_revolved_points(pts, axis, numsegments)
             index_array.push indices
         end
     end
-
+    
     # Now create polygons using the point indices
     i1 = index_array[0]
     for i in 1...numpts do
@@ -74,7 +65,7 @@ def add_revolved_points(pts, axis, numsegments)
         n1 = i1.length
         n2 = i2.length
         nest if( n1 < numsegments && n2 < numsegments )
-
+        
         for j in 0...numsegments do
             jp1 = (j + 1) % numsegments
             if( n1 < numsegments )
@@ -94,10 +85,10 @@ def add_revolved_points(pts, axis, numsegments)
                 #puts "add_poly #{i1[j]}, #{i1[jp1]}, #{i2[jp1]}, #{i2[j]}"
             end
         end
-
+        
         i1 = i2
     end
-
+    
 end
 
 # Extrude points along an axis with a rotation
@@ -118,7 +109,7 @@ def add_extruded_points(pts, center, dir, angle, numsegments)
     t = Geom::Transformation.translation vec
     r = Geom::Transformation.rotation center, dir, da
     tform = t * r
-
+    
     # Add the points to the mesh
     index_array = []
     for i in 0...numsegments do
@@ -129,21 +120,21 @@ def add_extruded_points(pts, center, dir, angle, numsegments)
         end
         index_array.push indices
     end
-
+    
     # Now create polygons using the point indices
     i1 = index_array[0]
     for i in 1...numsegments do
         i2 = index_array[i]
-
+        
         for j in 0...numpts do
             k = (j+1) % numpts
             self.add_polygon -i1[j], i2[k], -i1[k]
             self.add_polygon i1[j], -i2[j], -i2[k]
         end
-
+        
         i1 = i2
     end
-
+    
 end
 
 end # Geom::PolygonMesh
@@ -162,9 +153,9 @@ def self.revolve_test(num)
     numpts = npts * num
     numpoly = (npts-1)*num
     mesh = Geom::PolygonMesh.new numpts, numpoly
-
+    
     mesh.add_revolved_points pts, axis, num
-
+    
     Sketchup.active_model.entities.add_faces_from_mesh mesh, 0
 end
 
@@ -179,12 +170,12 @@ def self.extrude_test(dist, angle, num)
     numpts = num * pts.length
     numpoly = (numpts - pts.length) * 2
     mesh = Geom::PolygonMesh.new numpts, numpoly
-
+    
     vec = Geom::Vector3d.new 0, 0, dist
     mesh.add_extruded_points pts, ORIGIN, vec, angle, num
 
     model.entities.add_faces_from_mesh mesh, 9
-
+    
     true
 end
 

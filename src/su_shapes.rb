c8c61378d78b247da569f732e6509619276a1cfd
thomthom <thomas@thomthom.net>
2014-03-24 11:39:43 +0100

    Cleaned up root RB file.

--- src/su_shapes.rb
+++ src/su_shapes.rb
@@ -13,15 +13,16 @@ module CommunityExtensions
   module Shapes
 
     # Create the extension.
-    shapes_ext = SketchupExtension.new("Shapes Tool",
-      File.join(File.dirname(__FILE__), 'su_shapes', 'shapes.rb'))
-    shapes_ext.description = "Shapes sample script from SketchUp.com"
-    shapes_ext.version =  "1.4.5"
-    shapes_ext.creator = "SketchUp"
-    shapes_ext.copyright = "2014, Trimble Navigation Limited and John W McClenahan"
+    loader = File.join(File.dirname(__FILE__), "su_shapes", "shapes.rb")
+    extension = SketchupExtension.new("Shapes Tool", loader)
+    extension.description = "Shapes sample script from SketchUp.com"
+    extension.version     = "1.4.5"
+    extension.creator     = "SketchUp"
+    extension.copyright   = "2014, Trimble Navigation Limited and " <<
+                            "John W McClenahan"
 
-    # Register the extension with Sketchup so it show up in the Preference panel.
-    Sketchup.register_extension shapes_ext, true
+    # Register the extension with so it show up in the Preference panel.
+    Sketchup.register_extension(extension, true)
 
   end # module Shapes
 end # module CommunityExtensions

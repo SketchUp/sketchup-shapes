6382811f71e974c03ce8d34424deb913090595c3
Thomas Thomassen <thomas@thomthom.net>
2014-04-25 07:45:33 +0200

    Update su_shapes.rb

--- src/su_shapes.rb
+++ src/su_shapes.rb
@@ -16,7 +16,7 @@ module CommunityExtensions
     loader = File.join(File.dirname(__FILE__), "su_shapes", "shapes.rb")
     extension = SketchupExtension.new("Shapes Tool", loader)
     extension.description = "Shapes sample script from SketchUp.com"
-    extension.version     = "2.0.0"
+    extension.version     = "2.0.1"
     extension.creator     = "SketchUp"
     extension.copyright   = "2014, Trimble Navigation Limited and " <<
                             "John W McClenahan"
